8d331f7082b929a0e82106751bdfe9e55b65e75a
Adnan Attar <73285966+adnanattar@users.noreply.github.com>
2022-12-12 16:49:00 +0530

    Initial Essential Screens

---
+++ app/src/main/java/com/tkiet/biobells/PatientPrescriptions.java
@@ -0,0 +1,14 @@
+package com.tkiet.biobells;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PatientPrescriptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_patient_prescriptions);
+    }
+}
